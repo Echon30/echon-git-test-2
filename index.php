d8e419c084c58267bfb100dbfdca9f74dd7978d5
Echon30 <marshandapatioran45@gmail.com>
2023-09-11 10:33:05 +0900

    menambhakan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quas dicta molestias sapiente consequuntur. Eius amet consequatur vitae voluptas placeat, iure sed deleniti quia doloribus dolore nostrum tempore. Architecto, vero?</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
